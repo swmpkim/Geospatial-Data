0a79fa6793a4f335b1dc5eb0a4d959e3c05c71cb
Kim Cressman <kc2741@msstate.edu>
2025-06-13 11:50:48 -0500

    added waterbody to processing file

--- process_EPANHD_toMS.R
+++ process_EPANHD_toMS.R
@@ -27,13 +27,14 @@ path_ms_shps <- here::here(path_ms_db,
                            "Hydrography")
 
 # Tennessee 06 region (northeast corner of MS)
-path_ne_db <- here::here("EPA NHD",
-                         "NHDPlusV21_MS_06_NHDSnapshot_09",
-                         "NHDPlusMS",
-                         "NHDPlus06",
-                         "NHDSnapshot")
-path_ne_shps <- here::here(path_ms_db,
-                           "Hydrography")
+# (didn't work out - repeat of MS files)
+# path_ne_db <- here::here("EPA NHD",
+#                          "NHDPlusV21_MS_06_NHDSnapshot_09",
+#                          "NHDPlusMS",
+#                          "NHDPlus06",
+#                          "NHDSnapshot")
+# path_ne_shps <- here::here(path_ms_db,
+#                            "Hydrography")
 
 # FCodes with descriptions 
 db_codes <- read.dbf(here::here(path_sa_db,
@@ -59,11 +60,11 @@ flowline_sa <- st_read(here::here(path_sa_shps,
 flowline_sa <- st_intersection(flowline_sa, ms_sf)
 gc()
 
-# read and clip NE corner flowlines ----
-flowline_ne <- st_read(here::here(path_ne_shps,
-                                  "NHDFlowline.shp"))
-flowline_ne <- st_intersection(flowline_ne, ms_sf)
-gc()
+# # read and clip NE corner flowlines ----
+# flowline_ne <- st_read(here::here(path_ne_shps,
+#                                   "NHDFlowline.shp"))
+# flowline_ne <- st_intersection(flowline_ne, ms_sf)
+# gc()
 
 # make sure it worked
 ggplot() +
@@ -73,13 +74,7 @@ ggplot() +
     geom_sf(data = flowline_sa,
             col = "blue") +
     geom_sf(data = flowline_ms,
-            col = "red") +
-    geom_sf(data = flowline_ne,
-            col = "purple")
-
-# it did not.... that's the same as the MS08 file
-rm(flowline_ne)
-gc()
+            col = "red")
 
 # combine two objects into one ----
 flowline_MS <- bind_rows(flowline_ms,
@@ -97,4 +92,75 @@ st_write(flowline_MS,
                     "MS_only",
                     "flowline_MS.gpkg"))
 
+
+
+####################################################
+
+
+# read and clip waterbody polygons ----
+polys_ms <- st_read(here::here(path_ms_shps,
+                                  "NHDWaterbody.shp"))
+# something is wrong with the geometries and they won't join
+sum(st_is_valid(polys_ms) == FALSE)
+inds <- which(st_is_valid(polys_ms) == FALSE)
+polys_ms[inds, ] |> 
+    st_is_valid(reason = TRUE)
+# lots of duplicate vertices
+
+# try making it valid
+polys_ms2 <- st_make_valid(polys_ms)
+sum(st_is_valid(polys_ms2) == FALSE)
+# that seems to have worked
+
+polys_ms <- st_intersection(polys_ms2, ms_sf)
+rm(polys_ms2)
+gc()
+
+
+# read and clip SA flowlines ----
+polys_sa <- st_read(here::here(path_sa_shps,
+                                  "NHDWaterbody.shp"))
+sum(st_is_valid(polys_sa) == FALSE)  # 7
+polys_sa2 <- st_make_valid(polys_sa)
+sum(st_is_valid(polys_sa2) == FALSE) # all good
+polys_sa <- st_intersection(polys_sa2, ms_sf)
+rm(polys_sa2)
+gc()
+
+# make sure it worked
+ggplot() +
+    geom_sf(data = ms_sf,
+            fill = "gray90",
+            col = "black") +
+    geom_sf(data = polys_sa,
+            fill = "blue",
+            col = NA) +
+    geom_sf(data = polys_ms,
+            fill = "red",
+            col = NA)
+
+# combine two objects into one ----
+# have to make the names the same first
+polys_ms <- janitor::clean_names(polys_ms)
+names(polys_ms) <- str_remove_all(names(polys_ms), "_")
+
+polys_sa <- janitor::clean_names(polys_sa)
+names(polys_sa) <- str_remove_all(names(polys_sa), "_")
+
+polys_MS <- bind_rows(polys_ms,
+                      polys_sa)
+ggplot() +
+    geom_sf(data = ms_sf,
+            fill = "gray90",
+            col = "black") +
+    geom_sf(data = polys_MS,
+            fill = "blue",
+            col = NA)
+
+# write out ----
+st_write(polys_MS,
+         here::here("EPA NHD",
+                    "MS_only",
+                    "waterbody_MS.gpkg"),
+         delete_dsn = TRUE)
 
